ecdd3aa905fb9f86e420402baec5922cef5b2277
balkon16 <pawel.k.lonca@gmail.com>
2018-12-08 15:35:28 +0100

    POST method present

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -29,17 +29,31 @@ def index():
 def register():
     return render_template("register.html")
 
+
 @app.route("/reset")
 def reset():
     return render_template("reset.html")
 
+### do usunięcia ####
 @app.route("/table")
 def table():
     users = db.execute("SELECT * FROM users").fetchall()
     return render_template("table.html", users=users)
+#####################
 
 @app.route("/logout")
 def logout():
     #trzeba dodać coś co wyłącza sesje użytkownika;
     #na razie mam tylko layout strony
     return render_template("logout.html")
+
+@app.route("/register_next", methods=["POST"])
+def register_next():
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    email = request.form.get("email")
+    username = request.form.get("username")
+    pwd = request.form.get("pwd")
+    pwd_rep = request.form.get("pwd_rep")
+    message = "test message"
+    return render_template("success.html", message=message)
